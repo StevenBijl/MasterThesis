121e4284686836f38aa4dd59ab365bf0c28afee4
StevenBijl <146464972+StevenBijl@users.noreply.github.com>
2023-10-23 10:09:54 +0200

    Delete Data Preprocessing/main.py

--- Data Preprocessing/main.py
+++
@@ -1,6 +0,0 @@
-# main.py
-if __name__ == '__main__':
-    with open('1.Binary_to_hdf.py', 'r') as file:
-        exec(file.read())
-    with open('1.hdf_to_usable_hdf.py', 'r') as file:
-        exec(file.read())
